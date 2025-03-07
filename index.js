ecf500a4d086f42a3cec3bce26e6ba34613317d4
marylyne <otienomarylyne76@gmail.com>
2025-03-07 15:41:47 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution in this file!
+var customerName = "bob";
+function returnCustomerName() {
+  return customerName;
+}
+
+
+var customerName = "bob";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+
+/*
+var bestCustomer= "not bob";
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+  */
+/*
+var bestCustomer;
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}*/
+
+
+
+const LeastFavoriteCustomer = "someone";
+function changeLeastFavoriteCustomer(){
+  LeastFavoriteCustomer = "someone else";
+}
+
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
